0b79c022340ca3249bd4d322f19da816ccfab930
kmskpraveen <bgkmskpraveenmb@gmail.com>
2020-02-18 14:19:47 +0530

    Mode option

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
 module.exports = {
     // this is actually the configuration webpack uses
+    
+    // This results a nice, verbose output in main.js 
+    mode: 'development'
+    // This results a compressed, performance oriented main.js
+    // mode: 'production'
 }
