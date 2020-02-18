a6cfff6bbf95c37d0de6e567348b61a24377aa45
kmskpraveen <bgkmskpraveenmb@gmail.com>
2020-02-18 14:39:37 +0530

    Output.filename option

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,11 @@ module.exports = {
 
     // This is the default path for the entry point
     // entry: './src/index.js'
-    entry: './src/engine.js'
+    entry: './src/engine.js',
     // Note: ypu can also specify multiple entry points like an object (or key-value pair)
+
+    // Specifies the name of .js file that resilted from $ npm run build and its location
+    output: {
+        filename: '../build/build.js'
+    } 
 }
